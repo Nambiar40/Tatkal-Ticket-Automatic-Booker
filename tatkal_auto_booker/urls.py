b33d58878c17ca65364a4babe45434db53030154
Nambiar40 <syedunandan@gmail.com>
2025-08-07 19:45:37 +0530

    Todays updates

--- tatkal_auto_booker/urls.py
+++ tatkal_auto_booker/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect  # ✅ you need this for redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('dashboard')),  # ✅ redirect root to /dashboard/
     path('admin/', admin.site.urls),
-    path('', include('booking.urls')),
+    path('dashboard/', include('booking.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
